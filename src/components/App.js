a5bebf80ea1e11d59c70af8f9653da609eca88b5
pratiknilopant <mr.pratiknilopant@gmail.com>
2023-08-13 10:28:22 +0530

    Final Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import Heading from './Heading';
+import InputQuery  from "./InputQuery";
+import SubHeading  from "./SubHeading";
+import SubmitBtn  from "./SubmitButton";
+
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <InputQuery />
+      <SubHeading />
+      <SubmitBtn />
     </div>
   )
 }
